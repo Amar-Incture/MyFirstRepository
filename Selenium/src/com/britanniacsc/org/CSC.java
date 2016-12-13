1c791be8f6d1d91898024a5f320df4ec918f2232
amar <amar@172.16.30.205>
2016-12-13 16:19:48 +0530

    Took the count of all the templates

--- Selenium/src/com/britanniacsc/org/CSC.java
+++ Selenium/src/com/britanniacsc/org/CSC.java
@@ -15,15 +15,24 @@ public class CSC {
 	WebDriver driver = new ChromeDriver();
 	driver.get("http://hrapps.britindia.com");
 	driver.manage().window().maximize();
-	Thread.sleep(10000);
+	Thread.sleep(15000);
 	driver.findElement(By.xpath("/html/body/div[1]/center/div/div[3]/form/div[1]/input")).sendKeys("balakrishnan@britindia.com");
 	driver.findElement(By.xpath("/html/body/div[1]/center/div/div[3]/form/div[2]/input")).sendKeys("123");
 	driver.findElement(By.xpath("/html/body/div[1]/center/div/div[3]/form/div[3]/button")).click();
-	Thread.sleep(7000);
+	Thread.sleep(15000);
 	driver.findElement(By.xpath("//*[@id='suboptions']/li[2]/div/a")).click();
-	Thread.sleep(5000);
+	Thread.sleep(15000);
 	List<WebElement> totalTemplates = driver.findElements(By.xpath("//*[@id='wrapper']/div[2]/div/div/div[2]/table/tbody/tr/td[1]"));
 	System.out.println(totalTemplates.size());
+	int count = 0;
+	for(WebElement ele : totalTemplates){
+		System.out.println(ele.getText());
+		if(ele.getText()=="/s"){
+			count+=1;
+		}
+		
+	}
+	System.out.println("Total number of templates with blank names ="+count);
 	driver.findElement(By.xpath("//*[@id='wrapper']/div[2]/div/div/div[2]/table/tbody/tr[2]/td[1]")).click();
 }
 }
