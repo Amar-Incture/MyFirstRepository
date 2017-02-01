2978448d07bbbc7477e188a40b5692d9ad10a1e3
amar <amar@172.16.30.166>
2017-02-01 20:51:55 +0530

    Completed till clicking on New Requisition button

--- Selenium/src/com/britanniacsc/org/LogInRecruitment.java
+++ Selenium/src/com/britanniacsc/org/LogInRecruitment.java
@@ -1,17 +1,24 @@
 package com.britanniacsc.org;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class LogInRecruitment {
-
+public WebDriver driver;
 	
-	@Test
-	public static void launchURL() throws InterruptedException{
+	@BeforeMethod
+	public void launchURL() throws InterruptedException{
 		System.setProperty("webdriver.chrome.driver","//Users//amar//Downloads//chromedriver");
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.get("dev.cherrywork.in:7009");
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
@@ -20,5 +27,23 @@ public class LogInRecruitment {
 		driver.findElement(By.xpath("/html/body/div[1]/center/div/div[3]/form/div[4]/button")).click();
 	}
 	
-
+	@Test
+	public void requisitionList() throws Exception{
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//*[@id='dropdownMenu1']")).click();
+		driver.findElement(By.xpath("//*[@id='page-container']/ng-include/nav/div[2]/div/table/tbody/tr/td[1]/ng-include/div/div/ul/li[1]/a")).click();
+		driver.findElement(By.xpath("//*[@id='rec']/li[1]/div/a")).click();
+	}
+	
+	@AfterMethod
+	public void createRequisition() throws InterruptedException{
+		Thread.sleep(5000);
+		driver.findElement(By.xpath(".//*[@id='wrapper']/div[2]/div/div/div[1]/div[1]/button")).click();
+//		Select requisitionType = new Select(driver.findElement(By.xpath("html/body/div[1]/div/div/div[2]/form/div/div/select")));
+//		requisitionType.selectByVisibleText("New");
+	}
+	
 }
+	
+
+
