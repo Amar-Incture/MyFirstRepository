a0e357090828e345fc5e25f43fa02cde3bfa20c3
amar <amar@172.16.30.106>
2017-03-23 11:57:08 +0530

    ReadExcelCode

---
+++ LearnExcel/src/com/excel/org/ReadExcel.java
@@ -0,0 +1,22 @@
+package com.excel.org;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcel {
+
+	public static void main(String[] args) throws Exception {
+		
+		FileInputStream fis = new FileInputStream("//Users//amar//Downloads//A-New-01007339.xlsx");
+		
+		XSSFWorkbook wb = new XSSFWorkbook(fis);
+		
+		XSSFSheet sheet1 = wb.getSheetAt(0);
+		
+		System.out.println(sheet1.getRow(0).getCell(0).getStringCellValue());
+	}
+
+}
